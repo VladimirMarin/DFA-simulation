7dbe3aa1585b45d173db23f954c8e4c92498376c
LaEme <marinvladimir.constantin@gmail.com>
2017-03-29 00:37:00 +0300

    My DFA is done.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <string>
 #include<stdlib.h>
 using namespace std;
 
@@ -29,36 +29,64 @@ int main()
 {
     string Q, E, F;
     char q0;
-     f>>Q>>E>>F>>q0;
+    f>>Q>>E>>F>>q0;
+    qsort(&(E[0]), E.length(), sizeof(char), int_sorter);
 
-    qsort(&(Q[0]), Q.length(), sizeof(char), int_sorter);
-    cout << Q << endl;
 
     char **myFunction;
-    myFunction = new char*[Q.length()];
-    for(unsigned i = 0 ; i < Q.length(); i++)
-    {
-        myFunction[i] = new char[E.length()];
-        for(unsigned j = 0 ; j < E.length() ; j++)
-            f>>myFunction[i][j];
-    }
+    myFunction = new char*['z' - 'a'];
 
-    for(unsigned i = 0 ; i < Q.length(); i++)
+    for(int i = 0 ; i <= ('z' - 'a'); i++)
     {
-        cout<< Q[i] << ":"  << endl;
-        for(unsigned j = 0 ; j < E.length() ; j++)
-            cout<< myFunction[i][j] << "  ";
-        cout << endl;
+        size_t found = E.find('a' + i);
+        if(found != string::npos)
+        {
+            myFunction[i] = new char[Q.length()];
+            for(unsigned j = 0 ; j < Q.length() ; j++)
+                f>>myFunction[i][j];
+        }
+
     }
+    /*
+    for(int i = 0 ; i <= 'z' - 'a'; i++)
+    {
+        size_t found = E.find('a' + i);
+        if(found != string::npos)
+        {
+            cout<< (char)('a' + i) << ":"  << endl;
+            for(unsigned j = 0 ; j < Q.length() ; j++)
+                 cout<< myFunction[i][j] << "  ";
+            cout << endl;
+        }
+
+    }*/
+
+
+
 
     unsigned i = 0;
-    string word;char a; bool isGood =true; char current = q0;
+    string word;char a; bool isGood =false; char current = q0;
     cin >> word;
     while(i < word.length())
     {
         a = word[i];
-        current = myFunction[(int)a][(int)current];
+
+        current = myFunction[(int)(current - 'a')][(int)( a - 'a')];
+        //cout << current << " " << a << endl;
+        i++;
+
+
 
     }
+    for(unsigned i = 0 ; i < F.length() ; i++)
+        if(current == F[i])
+        {
+            isGood = true;
+            break;
+        }
+    if(isGood)
+        cout << word << " is a valid word";
+    else
+        cout << word << "  is not a valid word";
     return 0;
 }
